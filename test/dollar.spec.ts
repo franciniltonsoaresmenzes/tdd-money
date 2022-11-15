4bb98919e77fc32727b0d29dd8e93bb0ed44f088
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2022-11-15 19:57:45 -0300

    test: base euro class

--- test/dollar.spec.ts
+++ test/dollar.spec.ts
@@ -1,4 +1,5 @@
 import { Dollar } from '@/dollar'
+import { Euro } from '@/euro'
 import { describe, expect, it } from 'vitest'
 
 describe('Dollar', () => {
@@ -6,6 +7,12 @@ describe('Dollar', () => {
     const five: Dollar = new Dollar(5)
     expect(five.times(2).equals(new Dollar(10))).toBeTruthy()
     expect(five.times(3).equals(new Dollar(15))).toBeTruthy()
+  })
+
+  it('should return 10 when five euro multiplied by two and 15 when multiplied bt three', () => {
+    const five: Euro = new Euro(5)
+    expect(five.times(2).equals(new Euro(10))).toBeTruthy()
+    expect(five.times(3).equals(new Euro(15))).toBeTruthy()
   })
 
   it('should return true when dollars of the same value compared', () => {
