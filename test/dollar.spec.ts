8e9b7ec46cb37f0032aa3e68c59bae21e7bdc8b8
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2022-11-15 19:38:57 -0300

    refactor: improve test description

--- test/dollar.spec.ts
+++ test/dollar.spec.ts
@@ -2,7 +2,7 @@ import { Dollar } from '@/dollar'
 import { describe, expect, it } from 'vitest'
 
 describe('Dollar', () => {
-  it('should return 10 when five dollar multiplied by two', () => {
+  it('should return 10 when five dollar multiplied by two and 15 when multiplied bt three', () => {
     const five: Dollar = new Dollar(5)
     expect(five.times(2).equals(new Dollar(10))).toBeTruthy()
     expect(five.times(3).equals(new Dollar(15))).toBeTruthy()
