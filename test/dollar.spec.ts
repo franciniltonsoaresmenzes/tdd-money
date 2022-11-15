6bcc90efdf339a501e2ccd1df04a1ab21259035f
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2022-11-15 19:37:20 -0300

    refactor: make amount private

--- test/dollar.spec.ts
+++ test/dollar.spec.ts
@@ -4,10 +4,8 @@ import { describe, expect, it } from 'vitest'
 describe('Dollar', () => {
   it('should return 10 when five dollar multiplied by two', () => {
     const five: Dollar = new Dollar(5)
-    let product: Dollar = five.times(2)
-    expect(product.amount).toEqual(10)
-    product = five.times(3)
-    expect(product.amount).toEqual(15)
+    expect(five.times(2).equals(new Dollar(10))).toBeTruthy()
+    expect(five.times(3).equals(new Dollar(15))).toBeTruthy()
   })
 
   it('should return true when dollars of the same value compared', () => {
