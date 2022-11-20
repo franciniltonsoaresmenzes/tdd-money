78d2c51d6687392d55936b886124cc9fe6c867f3
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2022-11-20 17:22:53 -0300

    test: currency and common construct

--- test/money.spec.ts
+++ test/money.spec.ts
@@ -10,17 +10,22 @@ describe('Money', () => {
 
   it('should correctly handle euro multiplication', () => {
     const five: Money = Money.euro(5)
-    expect(five.times(2).equals(new Euro(10))).toBeTruthy()
-    expect(five.times(3).equals(new Euro(15))).toBeTruthy()
+    expect(five.times(2).equals(new Euro(10, 'EUR'))).toBeTruthy()
+    expect(five.times(3).equals(new Euro(15, 'EUR'))).toBeTruthy()
   })
 
   it('should correctly handle money equality', () => {
-    expect(Money.dollar(5).equals(new Dollar(5))).toBeTruthy()
-    expect(Money.dollar(5).equals(new Dollar(6))).toBeFalsy()
+    expect(Money.dollar(5).equals(new Dollar(5, 'USD'))).toBeTruthy()
+    expect(Money.dollar(5).equals(new Dollar(6, 'USD'))).toBeFalsy()
 
     expect(Money.euro(5).equals(Money.euro(5))).toBeTruthy()
     expect(Money.euro(5).equals(Money.euro(6))).toBeFalsy()
 
     expect(Money.euro(5).equals(Money.dollar(5))).toBeFalsy()
+  })
+
+  it('should correctly handle currencies', () => {
+    expect(Money.dollar(1).currency()).toEqual('USD')
+    expect(Money.euro(1).currency()).toEqual('EUR')
   })
 })
