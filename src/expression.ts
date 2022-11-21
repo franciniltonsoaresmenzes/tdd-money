cc5e83eded53fefd3d90b7ebc451d8eb89ba8f52
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2022-11-20 22:37:05 -0300

    feat: implement missing operations

--- src/expression.ts
+++ src/expression.ts
@@ -2,6 +2,7 @@ import { Bank } from './bank'
 import { Money } from './money'
 
 export interface Expression {
- reduce (bank: Bank, to: string): Money
+  reduce (bank: Bank, to: string): Money
   plus (addend: Expression): Expression
+  times (multiplier: number): Expression
 }
